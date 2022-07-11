9894801f3e1e5523cbc97a67a61b42c8dac6341a
jadedec <106964384+jadedec@users.noreply.github.com>
2022-07-11 15:08:44 +0100

    english to morse

--- script.js
+++ script.js
@@ -4,20 +4,30 @@ const inputBox = document.querySelector(".display--input");
 const outputBox = document.querySelector(".display--output");
 const toEnglishButton = document.querySelector(".button--morseToEnglish");
 const toMorseButton = document.querySelector(".button--englishToMorse");
+const clearButton = document.querySelector(".button--clear");
 
-
+//english to morse function
 const englishToMorse = (englishStr) => {
     //convert string to array
-    const newArr = englishStr.toUpperCase().split("");
-    newArr.map(key => {
+    const morseTrans = englishStr.toUpperCase().split("").map((key) => {
         return morseCode[key];
-    }).join("/");
-    return newArr;
+    })
+    //convert array to string,separated by "/""
+    return morseTrans.join("/");
 }
 
+//page change by click
 const handleEnglishToMorse = () => {
     const result = englishToMorse(inputBox.value);
     outputBox.innerHTML = result;
 }
-
+//add event listener to the button
 toMorseButton.addEventListener("click", handleEnglishToMorse);
+
+
+//clear button
+const clearAll = () => {
+    inputBox.value = "";
+    outputBox.innerHTML = "";
+}
+clearButton.addEventListener("click", clearAll);
