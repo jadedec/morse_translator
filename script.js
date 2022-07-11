a849926380ca3b08d8c1c2655f66f5bfd370461b
jadedec <106964384+jadedec@users.noreply.github.com>
2022-07-11 14:51:00 +0100

    import morsecode

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
+import { morseCode } from "./morse.js";
+
 const inputBox = document.querySelector(".display--input");
 const outputBox = document.querySelector(".display--output");
+const toEnglishButton = document.querySelector(".button--morseToEnglish");
+const toMorseButton = document.querySelector(".button--englishToMorse");
+
+
+const englishToMorse = (englishStr) => {
+    //convert string to array
+    const newArr = englishStr.toUpperCase().split("");
+    newArr.map(key => {
+        return morseCode[key];
+    }).join("/");
+    return newArr;
+}
+
+const handleEnglishToMorse = () => {
+    const result = englishToMorse(inputBox.value);
+    outputBox.innerHTML = result;
+}
+
+toMorseButton.addEventListener("click", handleEnglishToMorse);
